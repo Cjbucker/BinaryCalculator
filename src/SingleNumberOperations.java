04322a43966d9a8335d86fdf8baf7ca5cc0f411b
Hunter <hunter@line98.dev>
2020-09-06 15:08:34 -0400

    Added basic library functions

---
+++ src/SingleNumberOperations.java
@@ -0,0 +1,8 @@
+public class SingleNumberOperations {
+    public int getSquare(int number) {
+        return (int) Math.pow(number, 2);
+    }
+    public int getSquareRoot(int number) {
+        return (int) Math.sqrt(number);
+    }
+}
