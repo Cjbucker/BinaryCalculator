6678e057f786c36c35375d93ee333fc76cc1d651
Hunter <hunter@line98.dev>
2020-09-07 15:13:54 -0400

    Added tests and ensures functions are working as expected.

--- src/SingleNumberOperations.java
+++ src/SingleNumberOperations.java
@@ -1,8 +1,12 @@
 public class SingleNumberOperations {
-    public static int getSquare(int number) {
-        return (int) Math.pow(number, 2);
+    public static String getSquare(int number) {
+        number = (int) Math.pow(number, 2);
+        return String.valueOf(number);
     }
-    public static int getSquareRoot(int number) {
-        return (int) Math.sqrt(number);
+    public static String getSquareRoot(int number) {
+//        Make number positive to handle square root with integers without handling unreal
+        number = Math.abs(number);
+        number = (int) Math.sqrt(number);
+        return String.valueOf(number);
     }
 }
